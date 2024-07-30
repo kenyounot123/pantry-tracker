1bdd2507584483d5b29a254d5d8c101a8f49be4d
kenyounot123 <kenlu519@gmail.com>
2024-07-29 22:31:16 -0400

    Override some of MUI default themes by creating a theme.ts file

---
+++ theme.ts
@@ -0,0 +1,22 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+import { Merriweather } from 'next/font/google';
+
+// Load the Merriweather font
+const merriweather = Merriweather({ subsets: ['latin'], weight: ['300', '400', '700', '900'] });
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#9A4700',
+    },
+    secondary: {
+      main: '#AB8A71',
+    },
+  },
+  typography: {
+    fontFamily: `${merriweather.style.fontFamily}`,
+  }
+});
+
+export default theme;
